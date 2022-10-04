2df00f1ebed97e2d7e21e4d47c510aa46a19c91e
Eduardo Silveiro <100838831+FS05-03@users.noreply.github.com>
2022-10-04 18:20:50 +0100

    Create MyRectangleTest.java
    teste

---
+++ test/pt/pa/adts/MyRectangleTest.java
@@ -0,0 +1,20 @@
+package pt.pa.adts;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyRectangleTest {
+
+    @Test
+    void isPerfect(){
+        assertTrue(false);
+    }
+
+    @Test
+    void isPerfect_true(){
+        MyRectangle rectangle = MyRectangle("One", );
+    }
+
+
+}
